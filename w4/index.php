5af83acaeeef37a5dadbe467c95950a60772d02a
Vadriannn <vadrianhartanto@gmail.com>
2026-09-03 16:14:21 +0700

    test

---
+++ w4/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h1>Home</h1>
+    </main>
+    <footer>
+        <p>Copyright &copy; 2022</p>
+    </footer>
+</body>
+
+</html>
